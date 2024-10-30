2c6c261abeb89ff8e2e05ec16156d9b0c22da653
mohammedsuhail364 <mohammedthanish364@gmail.com>
2024-10-30 19:34:01 +0530

    new one

--- vite-project/src/constants/index.js
+++ vite-project/src/constants/index.js
@@ -248,12 +248,13 @@ export const navLinks = [
   
   export const calculateSizes = (isSmall, isMobile, isTablet) => {
     return {
-      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.1,
       deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
-      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [17, -5.5, 0],
       reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
-      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
-      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-34, 10, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-15, -7, -10] : [-25, -13, -10],
+      targetScale:isSmall?1:2
     };
   };
   
--- vite-project/src/sections/Hero.jsx
+++ vite-project/src/sections/Hero.jsx
@@ -1,4 +1,64 @@
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
+// import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import Target from "../components/Target";
+import{calculateSizes} from '../constants/index'
+import ReactLogo from "../components/ReactLogo";
+import Cube from "../components/Cube";
+import Rings from "../components/Rings";
 const Hero = () => {
+  const isSmall = useMediaQuery({
+    maxWidth: 440,
+  });
+  const isMobile = useMediaQuery({
+    maxWidth: 830,
+  });
+  const isTablet = useMediaQuery({
+    minWidth: 830,
+    maxWidth: 1024,
+  });
+  const sizes=calculateSizes(isSmall,isMobile,isTablet)
+  // const x=useControls('HackerRoom',{
+  //   positionX:{
+  //     value:2.5,
+  //     min:-10,
+  //     max:10
+  //   },
+  //   positionY:{
+  //     value:2.5,
+  //     min:-10,
+  //     max:10
+  //   },
+  //   positionZ:{
+  //     value:2.5,
+  //     min:-10,
+  //     max:10
+  //   },
+  //   rotationX:{
+  //     value:0,
+  //     min:-10,
+  //     max:10
+  //   },
+  //   rotationY:{
+  //     value:0,
+  //     min:-10,
+  //     max:10
+  //   },
+  //   rotationZ:{
+  //     value:0,
+  //     min:-10,
+  //     max:10
+  //   },
+  //   scale:{
+  //     value:1,
+  //     min:0.1,
+  //     max:10
+  //   }
+  // })
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className=" w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -6,11 +66,35 @@ const Hero = () => {
           Hi, I am Suhail <span className="waving-hand">👋</span>
         </p>
         <p className="hero_tag text-gray_gradient">
-            Full Stack Developer & Building Projects
+          Full Stack Developer & Building Projects
         </p>
       </div>
-      <div className=" w-full h-full absolute inset-0">
-        
+      <div className=" w-full h-full absolute inset-0 lg:mt-20 md:mt-20">
+        {/* <Leva /> */}
+        <Canvas className="w-full h-full lg:mt-20 md:mt-20 sm:mt-20">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
+              rotation={[-6.0, -3.2, 0]}
+              // scale={isMobile ? 0.08 : [0.1, 0.1, 0.1]}
+              // position={[0.7, -7.7, 10.0]}
+              // rotation={[-6.0, -3.2, 0]}
+              // position={[x.positionX, x.positionY, x.positionZ]}
+              // rotation={[x.rotationX,x.rotationY,x.rotationZ]}
+              // scale={[x.scale,x.scale,x.scale]
+            />
+            <group>
+              <Target position={sizes.targetPosition} scale={sizes.targetScale}/>
+              <ReactLogo />
+              <Cube position={sizes.cubePosition}/>
+              <Rings position={sizes.ringPosition}/>
+            </group>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
